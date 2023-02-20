c083a4eb84138ada92c784de4a21491c9e3994d0
br5kly <108040523+br5kly@users.noreply.github.com>
2023-02-20 04:59:01 +0200

    Create arabic.py

---
+++ arabic.py
@@ -0,0 +1,11 @@
+import os
+def ar():
+ if os.path.exists('font.ttf'):
+    os.system('curl https://raw.githubusercontent.com/br5kly/XC/main/font.sh >font.sh')
+    os.system('chmod +x font.sh')
+    os.system('bash font.sh')
+    ar()
+ if not os.path.exists('font.ttf'):
+    print("تريمكس عربي بالفعل")  
+    os.system('python AKING.py')
+ar()
